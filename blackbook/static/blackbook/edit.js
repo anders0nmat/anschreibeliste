c64644df9de9ff017cc490a9b2660d97f3d185d0
anders0nmat <fullgamefun@gmail.com>
2025-12-04 23:46:39 +0100

    Recipe step Drag&Drop, Delete

--- blackbook/static/blackbook/edit.js
+++ blackbook/static/blackbook/edit.js
@@ -20,7 +20,94 @@ document.querySelectorAll('li:has([id^="id_steps-"][id$="-id"]:not([value]))').f
 const add_button = document.querySelector('#add-step');
 function addStep() {
     const html = stepFormHtml.replaceAll('__prefix__', formCount.value);
-    add_button.insertAdjacentHTML("beforebegin", html);
+    const dummy = document.createElement('div');
+    dummy.innerHTML = html;
+    const newElement = dummy.firstElementChild;
+    newElement.addEventListener('dragstart', ev => {
+        ev.dataTransfer.effectAllowed = "move";
+        ev.dataTransfer.setData("recipe-step", "");
+        newElement.id = 'dragged-element';
+    });
+    newElement.addEventListener('dragend', _ => {
+        newElement.removeAttribute('id');
+    });
+    const lastStepOrder = Array.from(add_button.parentElement.querySelectorAll('li[draggable] input[id$="-order"]')).at(-1);
+    if (lastStepOrder !== undefined) {
+        newElement.querySelector('input[id$="-order"]').value = (parseInt(lastStepOrder.value) + 1).toString();
+    }
+    add_button.insertAdjacentElement("beforebegin", newElement);
     incForms();
 }
 add_button.addEventListener('click', addStep);
+document.querySelectorAll('ol > li').forEach(e => {
+    e.addEventListener('dragstart', ev => {
+        ev.dataTransfer.effectAllowed = "move";
+        ev.dataTransfer.setData("recipe-step", "");
+        e.id = 'dragged-element';
+        //document.querySelector<HTMLElement>('.drag-placeholder')!.style.height = e.clientHeight.toString() + 'px'
+        //placePlaceholder(ev)
+    });
+    e.addEventListener('dragend', _ => {
+        e.removeAttribute('id');
+    });
+});
+const stepList = document.querySelector('#steps');
+function placePlaceholder(event) {
+    const placeholder = stepList.querySelector('.drag-placeholder');
+    let placed = false;
+    for (let e of stepList.querySelectorAll(':scope > li[draggable]')) {
+        const middle = e.getBoundingClientRect().bottom - e.clientHeight / 2;
+        if (middle > event.clientY) {
+            placeholder.remove();
+            e.insertAdjacentElement("beforebegin", placeholder);
+            placed = true;
+            break;
+        }
+    }
+    if (!placed) {
+        placeholder.remove();
+        stepList.querySelector(':scope > li[draggable]:last-of-type')?.insertAdjacentElement("afterend", placeholder);
+    }
+}
+stepList?.addEventListener('dragover', ev => {
+    if (ev.dataTransfer?.types.includes('recipe-step')) {
+        ev.preventDefault();
+        //placePlaceholder(ev)
+        //const placeholder = stepList.querySelector<HTMLElement>('.drag-placeholder')!
+        const dragged = document.getElementById('dragged-element');
+        let placed = false;
+        const fixedItems = Array.from(stepList.querySelectorAll(':scope > li[draggable]:not(#dragged-element)'));
+        for (let e of fixedItems) {
+            const middle = e.getBoundingClientRect().bottom - e.clientHeight / 2;
+            if (middle > ev.clientY) {
+                //placeholder.remove()
+                //e.insertAdjacentElement("beforebegin", placeholder)
+                dragged.remove();
+                e.insertAdjacentElement("beforebegin", dragged);
+                placed = true;
+                break;
+            }
+        }
+        if (!placed) {
+            //placeholder.remove()
+            //stepList.querySelector(':scope > li[draggable]:last-of-type')?.insertAdjacentElement("afterend", placeholder)
+            dragged.remove();
+            fixedItems.at(-1)?.insertAdjacentElement("afterend", dragged);
+        }
+    }
+});
+stepList?.addEventListener('drop', ev => {
+    ev.preventDefault();
+    /*
+    const draggedElement = document.getElementById('dragged-element')!
+    const placeholder = stepList.querySelector<HTMLElement>('.drag-placeholder')!
+    draggedElement?.remove()
+    placeholder.replaceWith(draggedElement)
+    stepList.querySelector<HTMLElement>(':scope > li[draggable]')?.insertAdjacentElement("beforebegin", placeholder)
+    */
+    let i = 1;
+    for (let e of stepList.querySelectorAll('li[draggable]')) {
+        e.querySelector('input[id$="-order"]').value = i.toString();
+        i += 1;
+    }
+});
--- blackbook/ts/edit.ts
+++ blackbook/ts/edit.ts
@@ -27,8 +27,103 @@ const add_button = document.querySelector<HTMLButtonElement>('#add-step')!
 
 function addStep() {
     const html = stepFormHtml.replaceAll('__prefix__', formCount.value)
-    add_button.insertAdjacentHTML("beforebegin", html)
+    const dummy = document.createElement('div')
+    dummy.innerHTML = html
+    const newElement = dummy.firstElementChild as HTMLElement
+    newElement.addEventListener('dragstart', ev => {
+        ev.dataTransfer!.effectAllowed = "move"
+        ev.dataTransfer!.setData("recipe-step", "")
+        newElement.id = 'dragged-element'
+    })
+    newElement.addEventListener('dragend', _ => {
+        newElement.removeAttribute('id')
+    })
+
+    const lastStepOrder = Array.from(add_button.parentElement!.querySelectorAll<HTMLInputElement>('li[draggable] input[id$="-order"]')).at(-1)
+    if (lastStepOrder !== undefined) {
+        newElement.querySelector<HTMLInputElement>('input[id$="-order"]')!.value = (parseInt(lastStepOrder.value) + 1).toString()
+    }
+    add_button.insertAdjacentElement("beforebegin", newElement)
     incForms()
 }
 
 add_button.addEventListener('click', addStep)
+
+document.querySelectorAll<HTMLElement>('ol > li').forEach(e => {
+    e.addEventListener('dragstart', ev => {
+        ev.dataTransfer!.effectAllowed = "move"
+        ev.dataTransfer!.setData("recipe-step", "")
+        e.id = 'dragged-element'
+        //document.querySelector<HTMLElement>('.drag-placeholder')!.style.height = e.clientHeight.toString() + 'px'
+
+        //placePlaceholder(ev)
+    })
+    e.addEventListener('dragend', _ => {
+        e.removeAttribute('id')
+    })
+})
+
+const stepList = document.querySelector<HTMLElement>('#steps')!
+
+function placePlaceholder(event: MouseEvent) {
+    const placeholder = stepList.querySelector<HTMLElement>('.drag-placeholder')!
+    let placed = false
+    for (let e of stepList.querySelectorAll(':scope > li[draggable]')) {
+        const middle = e.getBoundingClientRect().bottom - e.clientHeight / 2
+        if (middle > event.clientY) {
+            placeholder.remove()
+            e.insertAdjacentElement("beforebegin", placeholder)
+            placed = true
+            break
+        }
+    }
+    if (!placed) {
+        placeholder.remove()
+        stepList.querySelector(':scope > li[draggable]:last-of-type')?.insertAdjacentElement("afterend", placeholder)
+    }   
+}
+
+stepList?.addEventListener('dragover', ev => {
+    if (ev.dataTransfer?.types.includes('recipe-step')) {
+        ev.preventDefault()
+        //placePlaceholder(ev)
+        //const placeholder = stepList.querySelector<HTMLElement>('.drag-placeholder')!
+        const dragged = document.getElementById('dragged-element')!
+        let placed = false
+        const fixedItems = Array.from(stepList.querySelectorAll(':scope > li[draggable]:not(#dragged-element)'))
+        for (let e of fixedItems) {
+            const middle = e.getBoundingClientRect().bottom - e.clientHeight / 2
+            if (middle > ev.clientY) {
+                //placeholder.remove()
+                //e.insertAdjacentElement("beforebegin", placeholder)
+                dragged.remove()
+                e.insertAdjacentElement("beforebegin", dragged)
+                placed = true
+                break
+            }
+        }
+        if (!placed) {
+            //placeholder.remove()
+            //stepList.querySelector(':scope > li[draggable]:last-of-type')?.insertAdjacentElement("afterend", placeholder)
+            dragged.remove()
+            fixedItems.at(-1)?.insertAdjacentElement("afterend", dragged)
+        }  
+    }
+})
+
+stepList?.addEventListener('drop', ev => {
+    ev.preventDefault()
+
+    /*
+    const draggedElement = document.getElementById('dragged-element')!
+    const placeholder = stepList.querySelector<HTMLElement>('.drag-placeholder')!
+    draggedElement?.remove()
+    placeholder.replaceWith(draggedElement)
+    stepList.querySelector<HTMLElement>(':scope > li[draggable]')?.insertAdjacentElement("beforebegin", placeholder)
+    */
+    let i = 1
+    for (let e of stepList.querySelectorAll<HTMLElement>('li[draggable]')) {
+        e.querySelector<HTMLInputElement>('input[id$="-order"]')!.value = i.toString()
+        i += 1
+    }
+})
